a27ab38654bd4ae01451e7d9fae747bdfacf5593
lukewade2048 <luke.wade2048@gmail.com>
2025-01-11 16:25:52 +0000

    Updated docs and added Java example test

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Demo ALFA Fire (JSON Schema) Model Integration Project
-======================================================
+Demo ALFA and JSON Schema Model Integration Project
+===================================================
 
+Introduction
+------------
 This project shows how easy it is to:
 - Write type-safe Java code against JSON Schema imported into ALFA
 - Validate and create clean JSON conforming to the model
@@ -9,13 +11,25 @@ This project shows how easy it is to:
 
 Suade Fire (github.com/SuadeLabs/fire) is used as the example JSON Schema model.  
 
-Steps refresh to version of ALFA models based on the current Fire models
-------------------------------------------------------------------------
+Details
+-------
+
+- `src/main/alfa` contains ALFA definitions imported from Fire models
+- Branches of this project exist corresponding to Fire github project release tags - e.g. v24.10. 
+- ALFA Change Analyzer report is configured to run against the last release tag.
+- Once `mvn clean install` is run on this project.
+  - `target/generated-sources/changeanalyzer` - contains report of changes between the last release and current
+  - `target/generated-sources/java`- contains Java generated code, conforming to the ALFA model 
+  - `target/generated-sources/markdown` - contains Markdown documentation for the model, and deployed to < site >
+
+
+Steps to update ALFA models based on the current Fire models
+------------------------------------------------------------
 
 1. Open a terminal and run the following commands
 
 **Windows instructions:** 
-REVIEW
+TO BE REVIEWED
 ```
 mkdir -P model\imported
 cd model\imported
@@ -25,14 +39,17 @@ alfa -i jsonschema -o fire-in-alfa -s namespace=firemodel fire\v1-dev
 cd ..\..
 ```
 
-**Linux/Mac instructions:**
+**Unix instructions:**
 ```
 mkdir -p target/imported
 cd target/imported
+
 git clone https://github.com/SuadeLabs/fire.git
-#can be a tag instead of master
 cd fire
+
+# can be a tag instead of master
 git checkout tags/v24.10
+
 cd ../../..
 rm -rf src/main/resources/fire/
 mkdir -p src/main/resources/fire/
@@ -41,4 +58,6 @@ mv target/imported/fire/examples src/main/resources/fire/
 rm -rf target/imported
 ```
 
+2. Run `mvn install -P import-fire-schema` to run the JSON schema to ALFA importer.
+   This will overwrite ALFA definitions in `src/main/alfa`.
 
---
+++ src/test/java/com/schemarise/alfa/demo/jsonschema/JsonReaderTest.java
@@ -0,0 +1,81 @@
+package com.schemarise.alfa.demo.jsonschema;
+
+import com.schemarise.alfa.runtime.Alfa;
+import com.schemarise.alfa.runtime.AlfaRuntimeValiationException;
+import com.schemarise.alfa.runtime.codec.json.JsonCodecConfig;
+import fire.model.asset_liability;
+import fire.model.example;
+import fire.model.loan;
+import fire.model.loan_type;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonReaderTest {
+
+    /**
+     * Test that we can read a Fire JSON file and validate its contents
+     */
+    @Test
+    public void testReadLoan() throws IOException {
+        String pathStr = getClass().getResource("/").getPath() + "../../src/main/resources/fire/examples/bbl_loans.json";
+        InputStream is = Files.newInputStream(Paths.get(pathStr));
+
+        // Set the class type we are expecting
+        JsonCodecConfig dc = Alfa.jsonCodecConfigBuilder().setAssignableToClass(example.class).build();
+
+        // Read JSON and construct ALFA object
+        example obj = Alfa.jsonCodec().importObj(dc, is);
+
+        // Perform checks on what has been deserialized
+        Assertions.assertTrue( obj.getData().getLoan().isPresent() );
+
+        Set<loan> loans = obj.getData().getLoan().get();
+        assertEquals(3, loans.size());
+
+        loans.forEach( l -> {
+            assertEquals(l.getAsset_liability().get(), asset_liability.asset);
+            assertEquals(l.getType().get(), loan_type.commercial);
+        } );
+
+        // Create a new set of loans with fees set to 100
+        List<loan> loansWithFees = loans.stream().map(l -> {
+            loan.loanBuilder lb = l.toBuilder();
+            lb.setFees(100);
+            return lb.build();
+        }).toList();
+
+        // validate the fees
+        loansWithFees.forEach( l -> assertEquals(l.getFees().get(), 100));
+    }
+
+    @Test
+    public void failOnInvalidJson() throws IOException {
+
+        // Error in the JSON - "asset_liability": "set" <- typo in 'set', should 'asset'
+        var json = """
+                  {
+                    "date": "2020-08-08T00:00:00+00:00",
+                    "id": "BBL1",
+                    "asset_liability": "set",
+                    "balance": 2500000,
+                    "currency_code": "GBP"
+                  }""";
+
+        JsonCodecConfig dc = Alfa.jsonCodecConfigBuilder().setAssignableToClass(loan.class).build();
+
+        // Attempting to read should fail with a validation error
+        Exception excp = assertThrows( AlfaRuntimeValiationException.class, () -> Alfa.jsonCodec().fromJsonString(dc, json) );
+        assertEquals("Failed to process field 'asset_liability'. " +
+                        "Invalid value 'set' for type 'fire.model.asset_liability' at line:4 column:27 offset:83",
+                excp.getMessage() );
+    }
+}
